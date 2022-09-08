c9da986537cf1967b054c213017f27b69eb4a7b7
horoid <59495159+horoidrom@users.noreply.github.com>
2022-09-08 23:18:41 +0530

    main

---
+++ device_j1pop3g.mk
@@ -0,0 +1,193 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := device/samsung/j1pop3g
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Keylayouts
+KEYLAYOUT_FILES := \
+	$(LOCAL_PATH)/keylayouts/ist30xx_ts_input.kl \
+	$(LOCAL_PATH)/keylayouts/sci-keypad.kl
+
+PRODUCT_COPY_FILES += \
+	$(foreach f,$(KEYLAYOUT_FILES),$(f):system/usr/keylayout/$(notdir $(f)))
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+	setup_fs \
+	e2fsck \
+	f2fstat \
+	fsck.f2fs \
+	fibmap.f2fs \
+	mkfs.f2fs
+
+# Charger
+PRODUCT_PACKAGES += \
+	charger \
+	charger_res_images
+	
+# Bluetooth config
+PRODUCT_PACKAGES += \
+	libbluetooth_jni \
+	bluetooth.default \
+	bt_vendor.conf
+
+# Media config
+MEDIA_CONFIGS := \
+	$(LOCAL_PATH)/media/media_codecs.xml \
+	$(LOCAL_PATH)/media/media_profiles.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml
+
+PRODUCT_COPY_FILES += \
+	$(foreach f,$(MEDIA_CONFIGS),$(f):system/etc/$(notdir $(f)))
+
+# BT configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/bluetooth/iop_bt.db:system/etc/bluetooth/iop_bt.db \
+    $(LOCAL_PATH)/configs/bluetooth/iop_device_list.conf:system/etc/bluetooth/iop_device_list.conf
+
+# HWC
+PRODUCT_PACKAGES += \
+	gralloc.sc8830 \
+	hwcomposer.sc8830 \
+	sprd_gsp.sc8830 \
+	libion
+
+# Codecs
+PRODUCT_PACKAGES += \
+	libstagefrighthw \
+	libstagefright_sprd_soft_mpeg4dec \
+	libstagefright_sprd_soft_h264dec \
+	libstagefright_sprd_mpeg4dec \
+	libstagefright_sprd_mpeg4enc \
+	libstagefright_sprd_h264dec \
+	libstagefright_sprd_h264enc \
+	libstagefright_sprd_vpxdec \
+	libstagefright_sprd_aacdec \
+	libstagefright_sprd_mp3dec \
+	libomx_aacdec_sprd.so \
+	libomx_avcdec_hw_sprd.so \
+	libomx_avcdec_sw_sprd.so \
+	libomx_avcenc_hw_sprd.so \
+	libomx_m4vh263dec_hw_sprd.so \
+	libomx_m4vh263dec_sw_sprd.so \
+	libomx_m4vh263enc_hw_sprd.so \
+	libomx_mp3dec_sprd.so \
+	libomx_vpxdec_hw_sprd.so
+
+# Lights
+PRODUCT_PACKAGES += \
+	lights.sc8830
+
+# Device-specific packages
+PRODUCT_PACKAGES += \
+	SamsungServiceMode
+
+# Audio
+PRODUCT_PACKAGES += \
+	audio.r_submix.default \
+	audio.usb.default \
+	libaudio-resampler \
+	libtinyalsa \
+	audio.primary.sc8830 \
+	audio_policy.sc8830 \
+	audio_vbc_eq
+
+# Use prebuilt webviewchromium
+PRODUCT_PACKAGES += \
+	webview \
+	libwebviewchromium_loader.so \
+	libwebviewchromium_plat_support.so
+	
+# WPA supplicant
+PRODUCT_PACKAGES += \
+    hostapd \
+    libwpa_client \
+	dhcpcd.conf \
+	wpa_supplicant \
+	nvram_net.txt \
+	hostapd \
+	macloader
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wpa_supplicant/mcs.ini:system/etc/wifi/mcs.ini \
+    $(LOCAL_PATH)/configs/wpa_supplicant/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    $(LOCAL_PATH)/configs/wpa_supplicant/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wpa_supplicant/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
+
+# Permissions
+PERMISSION_XML_FILES := \
+	$(LOCAL_PATH)/permissions/platform.xml \
+	frameworks/native/data/etc/handheld_core_hardware.xml \
+	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml \
+	frameworks/native/data/etc/android.hardware.camera.front.xml \
+	frameworks/native/data/etc/android.hardware.camera.xml \
+	frameworks/native/data/etc/android.hardware.bluetooth_le.xml \
+	frameworks/native/data/etc/android.hardware.location.gps.xml \
+	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.xml \
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml \
+	frameworks/native/data/etc/android.software.sip.voip.xml \
+	frameworks/native/data/etc/android.software.sip.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.hardware.wifi.direct.xml
+
+PRODUCT_COPY_FILES += \
+	$(foreach f,$(PERMISSION_XML_FILES),$(f):system/etc/permissions/$(notdir $(f)))
+
+# Scripts
+SCRIPTS_FILES := \
+	$(LOCAL_PATH)/scripts/set_freq.sh
+	$(LOCAL_PATH)/scripts/zram.sh
+
+PRODUCT_COPY_FILES += \
+	$(foreach f,$(SCRIPTS_FILES),$(f):system/bin/$(notdir $(f)))
+
+# Set default USB interface
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=mtp
+
+# Device props
+PRODUCT_PROPERTY_OVERRIDES += \
+	keyguard.no_require_sim=true \
+	ro.kernel.android.checkjni=0 \
+	dalvik.vm.checkjni=false
+
+# ART device props
+PRODUCT_PROPERTY_OVERRIDES += \
+	dalvik.vm.dex2oat-flags=--no-watch-dog \
+	dalvik.vm.dex2oat-filter=everything \
+	dalvik.vm.image-dex2oat-filter=everything \
+	ro.sys.fw.dex2oat_thread_count=4
+
+# Support for Browser's saved page feature. This allows
+# for pages saved on previous versions of the OS to be
+# viewed on the current OS.
+PRODUCT_PACKAGES += \
+	libskia_legacy
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Set those variables here to overwrite the inherited values.
+PRODUCT_NAME := full_j1pop3g
+PRODUCT_DEVICE := j1pop3g
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SM-J110H
