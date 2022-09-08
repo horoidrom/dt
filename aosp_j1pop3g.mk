a189ac93fe95fbfe9452391f10c45db9b5ed8655
horoid <59495159+horoidrom@users.noreply.github.com>
2022-09-08 23:36:19 +0530

    Update aosp_j1pop3g.mk

--- aosp_j1pop3g.mk
+++ aosp_j1pop3g.mk
@@ -15,8 +15,7 @@
 #
 
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := j1pop3g
