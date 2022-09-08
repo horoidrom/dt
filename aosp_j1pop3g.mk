d83b5e391495d4d434e6b7dfaf629230991d5183
horoid <59495159+horoidrom@users.noreply.github.com>
2022-09-08 23:50:00 +0530

    Update aosp_j1pop3g.mk

--- aosp_j1pop3g.mk
+++ aosp_j1pop3g.mk
@@ -3,7 +3,5 @@
 # Inherit from jflte device
 $(call inherit-product, device/samsung/j1pop3g/full_j1pop3g.mk)
 
-# Inherit our stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 PRODUCT_NAME := aosp_j1pop3g
